a9fa57de81cfaaf3e3a02e5c32b698e16ca4582e
Md. Alamgir Hosen <64268610+fuck-boy-alam-vau-tera-papa@users.noreply.github.com>
2022-07-24 19:29:20 +0600

    Add files via upload

--- Crack.py
+++ Crack.py
@@ -1,1 +1,8 @@
-
+import os, platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from killerp import killerboy
+    killerboy()
+elif bit == '32bit':
+    exit('\x1b[1;91m\n\tOpps Your Device Not Supported')
